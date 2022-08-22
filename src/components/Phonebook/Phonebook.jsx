000bf16087deedd9007a4f59610aa4b2b7d2641e
Vladimir Vashchenko <dn090884vvm@gmail.com>
2022-08-22 13:51:53 +0300

    Finishing Homework

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -6,25 +6,6 @@ import PhonebookFilter from 'components/PhonebookFilter/PhonebookFilter';
 import ContactList from 'components/ContactList/ContactList';
 
 const Phonebook = () => {
-  //   const dispatch = useDispatch();
-
-  //   const filterWord = useSelector(state => state.phonebookReducer.filter);
-  //   const contacts = useSelector(state => state.phonebookReducer.contacts);
-
-  //   const gettedContacts = contacts;
-
-  //   const getFilteredContacts = () => {
-  //     const normalizedFilter = filterWord.toLowerCase();
-
-  //     return gettedContacts.filter(contact =>
-  //       contact.name.toLowerCase().includes(normalizedFilter)
-  //     );
-  //   };
-
-  //   const deleteContact = id => {
-  //     dispatch(phonebookActions.deleteContact(id));
-  //   };
-
   return (
     <>
       <Wrapper>
